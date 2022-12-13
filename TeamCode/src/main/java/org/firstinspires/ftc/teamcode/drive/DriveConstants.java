65a4b637ff7da1294fd3da2eff6134eb16ce33c9
QU1CK5ILV3R <william.p.clay@gmail.com>
2022-12-13 09:09:51 -0500

    mig 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -47,7 +47,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.8898; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 10.9; // in
+    public static double TRACK_WIDTH = 10.7; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -89,7 +89,7 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 25.23217263620446;
     public static double MAX_ACCEL = 25.23217263620446;
-    public static double MAX_ANG_VEL = Math.toRadians(132.63275229357797);
+    public static double MAX_ANG_VEL = 31.46;
     public static double MAX_ANG_ACCEL = Math.toRadians(132.63275229357797);
 
 
